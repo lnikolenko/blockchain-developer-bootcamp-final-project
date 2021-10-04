3d31529501ead2087eb791efd6e7a378cbd7ac0d
lnikolenko <nikki.nikolenko@gmail.com>
2021-10-03 20:03:37 -0500

    adding skeleton for smart contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
+
 contract SimpleStorage {
-  uint storedData;
+    uint storedData;
 
-  function set(uint x) public {
-    storedData = x;
-  }
+    function set(uint x) public {
+        storedData = x;
+    }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+    function get() public view returns (uint) {
+        return storedData;
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var CryptoUnion = artifacts.require("./CryptoUnion.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(CryptoUnion);
 };
