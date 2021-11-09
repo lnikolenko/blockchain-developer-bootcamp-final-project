2ff486d03203f64026201cc7bf49dcebffc34a7d
lnikolenko <nikki.nikolenko@gmail.com>
2021-11-08 22:52:39 -0600

    commenting out the deployment code

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+/*
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const dotenv = require("dotenv");
 dotenv.config();
-
 const mnemonic = process.env.MNEMONIC;
+*/
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -15,11 +16,13 @@ module.exports = {
       port: 7545,
       network_id: "5777",
     },
+    /*
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, process.env.INFURA_URL),
       network_id: 3, // Ropsten's network id
       gas: 5500000,
     },
+    */
   },
   compilers: {
     solc: {
