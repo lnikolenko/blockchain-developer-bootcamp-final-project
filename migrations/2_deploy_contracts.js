5a71415d4efc21b8dbfc66f29b465f51aa3c860a
lnikolenko <nikki.nikolenko@gmail.com>
2021-11-08 20:00:58 -0600

    deleting unused stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var CryptoUnion = artifacts.require("./CryptoUnion.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
   deployer.deploy(CryptoUnion);
 };
